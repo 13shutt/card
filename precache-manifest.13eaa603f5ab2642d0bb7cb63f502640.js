381f90633c152ab014d8fe0814dc3eb87291a835
Arsen Shuttov <shutt13a@gmail.com>
2019-05-02 00:14:01 +0300

    upd

---
+++ precache-manifest.13eaa603f5ab2642d0bb7cb63f502640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407b49d533f198b401c369760e089012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5be2ce86952b002fe14",
+    "url": "/static/css/main.c0cfcd21.chunk.css"
+  },
+  {
+    "revision": "61fb301253b2cca90dc1",
+    "url": "/static/js/2.39b5aaa9.chunk.js"
+  },
+  {
+    "revision": "d5be2ce86952b002fe14",
+    "url": "/static/js/main.44b9bc87.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2b6ae890f6c8e580a150946ebc1567ec",
+    "url": "/static/media/user.2b6ae890.jpg"
+  }
+]);
